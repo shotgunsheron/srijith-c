6a9257403ea7c9a090993baa38f0cf5243beeae0
Srijith C <67525056+shotgunsheron@users.noreply.github.com>
2023-03-30 19:28:31 -0400

    Update CMajorScale.ino

--- Arduino/CMajorScale.ino
+++ Arduino/CMajorScale.ino
@@ -1,13 +1,10 @@
-// Define the pins for the speaker and button
 int speakerPin = 9;
 
 void setup() {
-  // Set the speaker pin as an output
   pinMode(speakerPin, OUTPUT);
 }
 
 void loop() {
-  // Define the frequency of each note in the C major scale
   int c = 262;
   int d = 294;
   int e = 330;
@@ -17,7 +14,6 @@ void loop() {
   int b = 494;
   int c2 = 523;
   
-  // Play the C major scale using the tone() function and delay()
   tone(speakerPin, c);
   delay(500);
   tone(speakerPin, d);
